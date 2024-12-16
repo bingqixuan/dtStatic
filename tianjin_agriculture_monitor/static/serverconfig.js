0d632dadec349f355dc24da839c5294322c3483a
溪衍 <bingqixuan@vip.qq.com>
2024-12-16 20:50:50 +0800

    tianjin_agriculture_monitor

---
+++ tianjin_agriculture_monitor/static/serverconfig.js
@@ -0,0 +1,147 @@
+window.global_url = {
+  API_URL: 'http://127.0.0.1:3000',
+  MODEL_URL: './model/tileset.json',
+  // 监控配置
+  MONITOR_CONFIG: [
+    {
+      label: "设CODcr在线分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.15249275111007, 38.95527439635718, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '张佳',
+      area: '科研区'
+    },
+    {
+      label: "TP在线分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.1234571564235, 38.97509332775813, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '王瑞',
+      area: '保育区'
+    },
+    {
+      label: "五参数监测仪",
+      type: "system",
+      icon: 13,
+      point: [117.14316602510617, 38.96137283884687, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '张佳',
+      area: '科研区'
+    },
+    {
+      label: "溶解氧监测仪",
+      type: "system",
+      icon: 13,
+      point: [117.1491663277498, 38.96165225272396, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '张佳',
+      area: '科研区'
+    },
+    {
+      label: "ORP分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.15810796772006, 38.94796703272158, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '张佳',
+      area: '科研区'
+    },
+    {
+      label: "ORP分析仪",
+      type: "system",
+      icon: 13,
+      point: [117.18710987165471, 38.92970137167191, 0],
+      url: "",
+      origin: 1,
+      ins_date: '2022-04-23',
+      admin: '张佳',
+      area: '科研区'
+    },
+    {
+      label: "监控2",
+      type: "monitor",
+      icon: 13,
+      point: [117.13339102034477, 38.969787702274516, 0],
+      url: "",
+      origin: 4
+    },
+    {
+      label: "监控3",
+      type: "monitor",
+      icon: 13,
+      point: [117.15860615332423, 38.94446373582399, 0],
+      url: "",
+      origin: 1
+    },
+    {
+      label: "监控4",
+      type: "monitor",
+      icon: 13,
+      point: [117.1226172383957, 38.97913841581083, 0],
+      url: "",
+      origin: 1
+    },
+    {
+      label: "监控5",
+      type: "monitor",
+      icon: 13,
+      point: [117.13076605045343, 38.97562969369748, 0],
+      url: "",
+      danger: true,
+      origin: 2
+    },
+    {
+      label: "1号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.16416189582296, 38.94222449707036, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "2号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.15847972025885, 38.93898562532035, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "3号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.17656637333333, 38.92647099980325, 0],
+      url: "",
+      origin: 3
+    },
+    {
+      label: "4号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.18045289655892, 38.920090531836635, 0],
+      url: "",
+      danger: true,
+      origin: 1
+    },
+    {
+      label: "5号监测点",
+      type: "monitor",
+      icon: 13,
+      point: [117.16616425202886, 38.932036700046424, 0],
+      url: "",
+      origin: 2
+    }
+  ],
+};
